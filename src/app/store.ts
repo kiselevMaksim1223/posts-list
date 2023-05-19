f339299d9949ad84fb4e3152aa5687a86a731530
Maksim_Kiselev <laurkurt@gmail.com>
2023-05-19 23:17:11 +0300

    feature(store): reducers was added in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,19 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
 import { combineReducers, legacy_createStore } from 'redux'
+
+import { commentsReducer } from '../features/posts/post/comment/comments-reducer'
+import { postsReducer } from '../features/posts/posts-reducer'
 
 import { appReducer } from './app-reducer'
 
 const rootReducer = combineReducers({
   app: appReducer,
+  posts: postsReducer,
+  comments: commentsReducer,
 })
 
 export const store = legacy_createStore(rootReducer)
+
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+type AppRootStateType = ReturnType<typeof rootReducer>
