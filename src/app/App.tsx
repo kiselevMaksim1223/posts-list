6906d8639ed40e8170a5c675d0823d13d28d36f0
Maksim_Kiselev <laurkurt@gmail.com>
2023-05-19 23:19:46 +0300

    refactor(router): browserRouter replaced to index

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 
 import { About } from '../features/about/aboute'
 import { Header } from '../features/header/header'
@@ -11,19 +11,17 @@ import { UserDetails } from '../features/userDetails/userDetails'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className={'App'}>
-        <Header />
-        <Routes>
-          <Route path={'/'} element={<Navigate to={'/posts'} />} />
-          <Route path={'/posts'} element={<Posts />} />
-          <Route path={'/posts/:userId'} element={<UserDetails />} />
-          <Route path={'/about'} element={<About />} />
-          <Route path={'/404'} element={<div>Page not fount 404</div>} />
-          <Route path={'*'} element={<Navigate to={'/404'} />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className={'App'}>
+      <Header />
+      <Routes>
+        <Route path={'/'} element={<Navigate to={'/posts'} />} />
+        <Route path={'/posts'} element={<Posts />} />
+        <Route path={'/posts/:userId'} element={<UserDetails />} />
+        <Route path={'/about'} element={<About />} />
+        <Route path={'/404'} element={<div>Page not fount 404</div>} />
+        <Route path={'*'} element={<Navigate to={'/404'} />} />
+      </Routes>
+    </div>
   )
 }
 
