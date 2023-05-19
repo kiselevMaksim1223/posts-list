1c19a340acef05c6adbcda0ebad48f3f1ddc759f
Maksim_Kiselev <laurkurt@gmail.com>
2023-05-19 22:06:40 +0300

    feature(routes): add routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,26 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
+import { About } from '../features/about/aboute'
 import { Header } from '../features/header/header'
 import { Posts } from '../features/posts/posts'
+import { UserDetails } from '../features/userDetails/userDetails'
 
 function App() {
   return (
     <BrowserRouter>
       <div className={'App'}>
         <Header />
-        <Posts />
+        <Routes>
+          <Route path={'/'} element={<Navigate to={'/posts'} />} />
+          <Route path={'/posts'} element={<Posts />} />
+          <Route path={'/posts/:userId'} element={<UserDetails />} />
+          <Route path={'/about'} element={<About />} />
+          <Route path={'/404'} element={<div>Page not fount 404</div>} />
+          <Route path={'*'} element={<Navigate to={'/404'} />} />
+        </Routes>
       </div>
     </BrowserRouter>
   )
